27af969ca341cacccdcf1802e85187ef1514b74f
Stephanie <stephelle83@yahoo.fr>
2020-12-04 11:34:02 +0100

    recipeAdd2: Needed informations for the add form send in json

--- backend-ocooking/src/Controller/Api/V1/RecipeController.php
+++ backend-ocooking/src/Controller/Api/V1/RecipeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Api\V1;
 
+use App\Repository\CategoryRepository;
+use App\Repository\IngredientRepository;
+use App\Repository\MeasureRepository;
+use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,5 +16,33 @@ use Symfony\Component\Routing\Annotation\Route;
      */
 class RecipeController extends AbstractController
 {
-   
+   /**
+    * @Route("/add", name="needed_information_add", methods={"GET"})
+    */
+    public function neededInformationAdd(CategoryRepository $category, IngredientRepository $ingredient, MeasureRepository $measure, TagRepository $tag): Response
+    {
+        $categories = $category->findAll();
+        $ingredients = $ingredient->findAll();
+        $measures = $measure->findAll();
+        $tags = $tag->findAll();
+
+        return $this->json([
+            'categories' => $categories,
+            'ingredients' => $ingredients,
+            'measures' => $measures,
+            'tags' => $tags,
+        ]);
+    }
+
+    /**
+    * @Route("/add", name="add", methods={"POST"})
+    */
+    public function add(Request $request)
+    {
+        $json = $request->getContent();
+
+        $RecipeInformationsArray = json_decode($json, true);
+
+        dd($RecipeInformationsArray);
+    }
 }
