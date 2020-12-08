ee1c7c062f3fc1b8ae9d202266d5425fd41d8eb0
Slim <slim.benouada@gmail.com>
2020-12-08 13:53:31 +0100

    addition of the shopping list component

--- front/src/components/ShoppingList/index.js
+++ front/src/components/ShoppingList/index.js
@@ -1,29 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Checkbox } from 'semantic-ui-react'
 import './shoppingList.scss';
 
-const ShoppingList = () => (
-<>
-  <div className="app">
-    <input type="checkbox" id="scales" name="scales"
-    checked></input>
-    <label for="scales">Scales</label>
-  </div>
+const ShoppingList = () => {
+  console.log('ShoppingList');
+  return (
+    <>
+      <div className="home">
+        <h2 className="home__title">Liste de courses</h2>
+      </div>
+      <div className="home__miniature">
+        <div className="ui card">
+          <div className="liste_recettes">
+            <p className="recette">Recette 1
+              <button type="button" className="icon__addshopping">
+                <i className="trash alternate icon" />
+              </button>
+              <div className="content">
+                <Checkbox label="Article 1" />
+                <Checkbox label="Article 2" />
+                <Checkbox label="Article 3" />
+              </div>
+            </p>
+            <p className="recette">Recette 2
+              <button type="button" className="icon__addshopping">
+                <i className="trash alternate icon" />
+              </button>
+              <div className="content">
+                <Checkbox label="Article 1" />
+                <Checkbox label="Article 2" />
+                <Checkbox label="Article 3" />
+              </div>
+            </p>
+            <p className="recette">Recette 3
+              <button type="button" className="icon__addshopping">
+                <i className="trash alternate icon" />
+              </button>
+              <div className="content">
+                <Checkbox label="Article 1" />
+                <Checkbox label="Article 2" />
+                <Checkbox label="Article 3" />
+              </div>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-  <div>
-    <input type="checkbox" id="horns" name="horns"></input>
-    <label for="horns">Horns</label>
-  </div>
-  <div>
-    <input type="checkbox" id="horns" name="horns"></input>
-    <label for="horns">Horns</label>
-  </div>
-  <div>
-    <input type="checkbox" id="horns" name="horns"></input>
-    <label for="horns">Horns</label>
-  </div>
-
-</>
-
-);
 
 export default ShoppingList;
