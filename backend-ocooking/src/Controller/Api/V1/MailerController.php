304ef5f8aa7e6573652a6cc6c9813d5e164a84a9
renanbisig <bisigrenan@gmail.com>
2020-12-09 16:06:48 +0100

    first commit whit mailerController

---
+++ backend-ocooking/src/Controller/Api/V1/MailerController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Api\V1;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/api/v1/email", name="mailer", methods={"POST"})
+     */
+    public function sendEmail(MailerInterface $mailer):Void
+    {
+        $email = (new Email())
+            ->from('renan76@sfr.fr')
+            ->to('bisigrenan@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Test')
+            ->text('Sending emails is fun again!')
+            // ->html('<p>See Twig integration for better HTML integration!</p>')
+            ;
+
+        //  dd($email);
+        try {
+            $mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            // some error prevented the email sending; display an
+            // error message or try to resend the message
+        }
+    }
+}
