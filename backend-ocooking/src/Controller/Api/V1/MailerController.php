42a29afeb9c7efeaa6c21ba9084441abf5453552
renanbisig <bisigrenan@gmail.com>
2020-12-09 17:58:40 +0100

    search instal mailer

--- backend-ocooking/src/Controller/Api/V1/MailerController.php
+++ backend-ocooking/src/Controller/Api/V1/MailerController.php
@@ -3,18 +3,24 @@
 namespace App\Controller\Api\V1;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
+
 class MailerController extends AbstractController
 {
     /**
      * @Route("/api/v1/email", name="mailer", methods={"POST"})
      */
-    public function sendEmail(MailerInterface $mailer)
+    public function sendEmail(MailerInterface $mailer): void
     {
+        $transport = new EsmtpTransport('localhost');
+        $mailer = new Mailer($transport);
+
         $email = (new Email())
             ->from('renan76@sfr.fr')
             ->to('bisigrenan@gmail.com')
@@ -29,7 +35,7 @@ class MailerController extends AbstractController
 
         //  dd($email);
         try {
-            return $mailer->send($email);
+            $mailer->send($email);
         } catch (TransportExceptionInterface $e) {
             // some error prevented the email sending; display an
             // error message or try to resend the message
