c3f80311c9b4b63a27f7e92e9d1db1f23a7c4ef5
Kevin Conversin <conversin.k@gmail.com>
2020-12-09 09:39:25 +0100

    AddRecipeForm, removing the tags options in the creation

--- front/src/middlewares/recipe.js
+++ front/src/middlewares/recipe.js
@@ -9,18 +9,6 @@ const user = (store) => (next) => (action) => {
   switch (action.type) {
     case 'recipe/submit': {
       const { recipe } = store.getState();
-      console.log({
-        name: recipe.recipeName,
-        picture: recipe.recipeImage,
-        nbPeople: recipe.nbPerson * 1,
-        preparationTime: formatTime(recipe.preparationTime1, recipe.preparationTime2),
-        cookingTime: formatTime(recipe.cookingTime1, recipe.cookingTime2),
-        category: {
-          name: recipe.selectedCategory,
-        },
-        recipeIngredients: formatIG(recipe.ingredients),
-        steps: formatStep(recipe.steps),
-      });
       axios.post('http://localhost:8000/api/v1/recipes/add', {
         name: recipe.recipeName,
         picture: '',
