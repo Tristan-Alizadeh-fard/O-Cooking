e2a0ee2c298618f67a8d6aaa4514ba1a682d62f6
Stephanie <stephelle83@yahoo.fr>
2020-12-10 18:06:17 +0100

    Email to visitor when registering

--- backend-ocooking/src/Service/MailerService.php
+++ backend-ocooking/src/Service/MailerService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Entity\Recipe;
+use App\Entity\User;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
@@ -27,7 +28,15 @@ class MailerService
     }
 
     /**
-     * Send email (main method)
+     * Main method of the this service (all other methods call this one)
+     *
+     * @param string $subject
+     * @param string $from
+     * @param string $to
+     * @param string $template
+     * @param array $parameters
+     * @param string $headers
+     * @return void
      */
     public function send(string $subject, string $from, string $to, string $template, array $parameters, string $headers): void
     {
@@ -49,16 +58,37 @@ class MailerService
         $this->mailer->send($email);
     }
 
-    public function sendAlertAboutSignalRecipe(Recipe $recipe, string $from)
+    /**
+     * Send an Email to ocooking.contact@gmail.com when a recipe is signaled
+     *
+     * @param Recipe $recipe
+     * @param string $from
+     * @return void
+     */
+    public function sendAlertAboutSignalRecipe(Recipe $recipe, string $from): void
     {
         
         $subject = 'Recette ' . $recipe->getName() . ' (' . $recipe->getId() . ') signalée !';
         $template = 'email/adminSignaledRecipe.html.twig';
         $parameters = [
             'recipe' => $recipe,
+            'signaledBy' => $from,
         ];
         $headers = 'false';
 
         $this->send($subject, $from, 'ocooking.contact@gmail.com', $template, $parameters, $headers);
+    }
+
+    public function sendConfirmationInscriptiontoUser(User $user)
+    {
+        $subject = 'Bienvenue sur O\'Cooking';
+        $to = $user->getEmail();
+        $template = 'email/confirmInscription.html.twig';
+        $parameters = [
+            'user' => $user,
+        ];
+        $headers = 'true';
+
+        $this->send($subject, 'ocooking.contact@gmail.com', $to, $template, $parameters, $headers);
     }
 }
