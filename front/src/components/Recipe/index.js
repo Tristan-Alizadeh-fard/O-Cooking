b38eb190498b816d2cb5bc20cb08756baa98d839
Slim <slim.benouada@gmail.com>
2020-12-14 17:21:00 +0100

    work in progress

--- front/src/components/Recipe/index.js
+++ front/src/components/Recipe/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes, { string } from 'prop-types';
 import { Link } from 'react-router-dom';
+import testimage from 'src/pictures/sandwich.jpg';
 import './recipe.scss';
 import { setAllLoaders } from '../../actions/user';
 
@@ -14,38 +15,40 @@ const Recipe = ({ recipe, isLoading, setSignaled, setFavorite, unsetFavorite, ad
           <div className="ui text loader">Loading</div>
         </div>
         </div>}
-      {!isLoading && <div className="recette">
-        <div className="image__container">
-          <i className="image icon" />
-        </div>
-        <button type="button" className="icon__addshopping" onClick={() => addShopList(recipe.id)}>Ajouter à l'aide course
-          <i className="shopping cart icon" />
-        </button>
-        <button type="button" className="icon__addshopping">Share
-          <i className="paper plane icon" />
-        </button>
-        <button type="button" className="icon__addshopping">PDF
-          <i className="file pdf icon" />
-        </button>
-        {!favorite.find(fav => fav.name === recipe.name) && <button type="button" className="icon__addshopping" onClick={() => setFavorite(recipe.id)}>Ajouter aux favoris
-          <i className="heart icon" />
-        </button>}
-        {favorite.find(fav => fav.name === recipe.name) && <button type="button" className="icon__addshopping" onClick={() => unsetFavorite(recipe.id)}>Retirer des favoris
-          <i className="heart outline icon" />
-        </button>}
-        <button type="button" className="icon__addshopping" onClick={() => setSignaled(recipe.id)}>Signaler la recette
-          <i className="flag icon" />
-        </button>
-        <div className="recette__title">
-          <h2>{recipe.name}</h2>
+      {!isLoading && 
+        <div className="container">
+          <div className="image__button">
+            <div className="image">
+              <img src={testimage} className="image" alt="Table Setting" />
+            </div>
+            <div className="button">
+              <button type="button" className="icon__addshopping" onClick={() => addShopList(recipe.id)}>Ajouter à l'aide course
+                <i className="shopping cart icon" />
+              </button>
+              <button type="button" className="icon__addshopping">Share
+                <i className="paper plane icon" />
+              </button>
+              {!favorite.find(fav => fav.name === recipe.name) && <button type="button" className="icon__addshopping" onClick={() => setFavorite(recipe.id)}>Ajouter aux favoris
+                <i className="heart icon" />
+              </button>}
+              {favorite.find(fav => fav.name === recipe.name) && <button type="button" className="icon__addshopping" onClick={() => unsetFavorite(recipe.id)}>Retirer des favoris
+                <i className="heart outline icon" />
+              </button>}
+              <button type="button" className="icon__addshopping" onClick={() => setSignaled(recipe.id)}>Signaler la recette
+                <i className="flag icon" />
+              </button>
+            </div>
+         </div>
+        <div className="content">
+        
+          <h2 className="title">{`${recipe.name} Author: ${recipe.author.pseudo}`}</h2>
           {favorite.find(fav => fav.name === recipe.name) && <h3 className="favorite">Cette recette est dans vos favoris</h3>}
           {recipe.tags.map((tag) => (
-                    <div key={tag.id} className="tags__container">
-                      <span className="tag">{tag.name}</span>
-                    </div>
-                  ))}
+          <div key={tag.id} className="tags__container">
+            <span className="tag">{tag.name}</span>
+          </div>))}
           {recipe.signaled && <h3 className="signaled">Signalement confirmé - Recette potentielement douteuse ! - En attente de validation par l'admin</h3>}
-          <p className="author">{`By ${recipe.author.pseudo}`}</p>
+          
           <h3>{`- ${recipe.category.name} -`}</h3>
           <h3>{`- Temps de préparation = ${recipe.preparationTime}`}</h3>
           <h3>{`- Temps de cuisson = ${recipe.cookingTime}`}</h3>
@@ -62,7 +65,8 @@ const Recipe = ({ recipe, isLoading, setSignaled, setFavorite, unsetFavorite, ad
             <p key={step.id}>{step.description}</p>
           ))}
         </div>
-      </div>}
+      </div>
+    }
     </>
   );
 };
@@ -78,3 +82,58 @@ Recipe.prototype = {
 };
 
 export default Recipe;
+
+
+
+
+
+
+{/* <div className="recette">
+<div className="image__container">
+  <i className="image icon" />
+</div>
+<button type="button" className="icon__addshopping" onClick={() => addShopList(recipe.id)}>Ajouter à l'aide course
+  <i className="shopping cart icon" />
+</button>
+<button type="button" className="icon__addshopping">Share
+  <i className="paper plane icon" />
+</button>
+<button type="button" className="icon__addshopping">PDF
+  <i className="file pdf icon" />
+</button>
+{!favorite.find(fav => fav.name === recipe.name) && <button type="button" className="icon__addshopping" onClick={() => setFavorite(recipe.id)}>Ajouter aux favoris
+  <i className="heart icon" />
+</button>}
+{favorite.find(fav => fav.name === recipe.name) && <button type="button" className="icon__addshopping" onClick={() => unsetFavorite(recipe.id)}>Retirer des favoris
+  <i className="heart outline icon" />
+</button>}
+<button type="button" className="icon__addshopping" onClick={() => setSignaled(recipe.id)}>Signaler la recette
+  <i className="flag icon" />
+</button>
+<div className="recette__title">
+  <h2>{recipe.name}</h2>
+  {favorite.find(fav => fav.name === recipe.name) && <h3 className="favorite">Cette recette est dans vos favoris</h3>}
+  {recipe.tags.map((tag) => (
+            <div key={tag.id} className="tags__container">
+              <span className="tag">{tag.name}</span>
+            </div>
+          ))}
+  {recipe.signaled && <h3 className="signaled">Signalement confirmé - Recette potentielement douteuse ! - En attente de validation par l'admin</h3>}
+  <p className="author">{`By ${recipe.author.pseudo}`}</p>
+  <h3>{`- ${recipe.category.name} -`}</h3>
+  <h3>{`- Temps de préparation = ${recipe.preparationTime}`}</h3>
+  <h3>{`- Temps de cuisson = ${recipe.cookingTime}`}</h3>
+</div>
+<div className="recette__ingredients">
+  <h3 className="recette__ingredient">Liste des ingrédients</h3>
+  {recipe.recipeIngredients.map((ingredient) => (
+    <p key={ingredient.id}>{ingredient.quantity}</p>
+  ))}
+</div>
+<div className="recette__etapes">
+  <h3 className="recette__etape">Liste des étapes</h3>
+  {recipe.steps.map((step) => (
+    <p key={step.id}>{step.description}</p>
+  ))}
+</div>
+</div> */}
