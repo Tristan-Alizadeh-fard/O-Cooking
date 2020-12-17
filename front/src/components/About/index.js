92bc36215693abd54402f8c1188a8c81542b96bc
renanbisig <bisigrenan@gmail.com>
2020-12-17 22:51:35 +0100

    reset css of about

--- front/src/components/About/index.js
+++ front/src/components/About/index.js
@@ -18,43 +18,54 @@ const About = () => (
             <div className="picture">
               <img src="https://ca.slack-edge.com/T016B3JM5J5-U01CCBFM2FQ-2eac481aefd0-512" alt="" />
             </div>
-            <div>
-              <p>Kévin | React</p>
-              <p></p>
+            <div className="description">
+              <p>Kévin</p>
+              <p>React</p>
+              <p>Lead Dev Front</p>
             </div>
           </div>
           <div className="affiche_card">
             <div className="picture">
-              <img src="https://ca.slack-edge.com/T016B3JM5J5-U01677PF1AP-3d3fcd6f8061-512" alt="" />
+              <img src="https://ca.slack-edge.com/T016B3JM5J5-U01677PF1AP-80842f306660-192" alt="" />
             </div>
-            <p>Renan | Symfony</p>
+            <div className="description">
+              <p>Renan</p>
+              <p>Symfony</p>
+              <p>Srum Master</p>
+            </div>
           </div>
           <div className="affiche_card">
             <div className="picture">
               <img src="https://ca.slack-edge.com/T016B3JM5J5-U016ETQLUUU-26460626ad4f-512" alt="" />
             </div>
-            <p>Slim | React</p>
+            <div className="description">
+              <p>Slim</p>
+              <p>React</p>
+              <p>Git Master</p>
+            </div>
           </div>
           <div className="affiche_card">
             <div className="picture">
               <img src="https://ca.slack-edge.com/T016B3JM5J5-U01678JKA82-5524d0573566-512" alt="" />
             </div>
-            <p>Stéphanie | Symfony</p>
+            <div className="description">
+              <p>Stéphanie</p>
+              <p>Symfony</p>
+              <p>Product Owner - Lead Dev Back</p>
+            </div>
           </div>
           <div className="affiche_card">
             <div className="picture">
               <img src="https://ca.slack-edge.com/T016B3JM5J5-U016KTL660Z-12bee7b854b3-512" alt="" />
             </div>
-            <p>Tristan | React</p>
+            <div className="description">
+              <p>Tristan</p>
+              <p>React</p>
+              <p>Dév</p>
+            </div>
           </div>
         </div>
       </div>
-      {/* <div className="pictures">
-        <img className="php" src={php} alt="" />
-        <img className="symphony" src={symphony} alt="" />
-        <img className="react" src={react} alt="" />
-        <img className="js" src={js} alt="" />
-      </div> */}
     </div>
   </>
 );
